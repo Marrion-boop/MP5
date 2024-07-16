08d3f3ea4087e666f197f65f80e56c9528be1cf0
nak-ch <unreal4rafie@gmail.com>
2024-07-16 19:48:51 +0800

    Add files via upload

---
+++ test menu.py
@@ -0,0 +1,105 @@
+import test_user_info
+import os
+
+
+print("Welcome to the auction house")
+
+
+
+while True:
+    print("[1] Buy")
+    print("[2] Sell")
+    print("[3] Auction Winners")
+    print("[4] Feedback")
+    print("[5] Exit")
+    MenuChoice = int(input("Choice: "))
+
+    if MenuChoice == 1:
+        while True:
+            print("[1] Login")
+            print("[2] Sign-up")
+            SubMenuChoice = int(input("Choice: "))
+
+            if SubMenuChoice == 1:
+               dummy_name = input("Enter Username: ")
+               dummy_passinput = input("Enter Password: ")
+
+               for line in open("buyer_db.txt", "r").readlines():
+                    print(line)
+                    signin_check = line.split()
+                    
+                    if dummy_name == signin_check[1] and dummy_pass == signin_check[2]:
+                        os.system('cls')
+                        print("Login Successful")
+                        break
+                    else:
+                        os.system('cls')
+                        print("Login Failed")
+
+        
+            elif SubMenuChoice == 2:
+                #WALA PA MEMBER ID ITERATION
+                dummy_email = input("Enter your E-mail: ")
+                dummy_name = input("Enter your desired username: ")
+                dummy_pass = input("Enter your password: ")
+                dummy_address = input("Enter your address: ")
+                dummy_phonenum = input("Enter your phone number: ")
+                dummy_ship = input("Enter your shipping address: ")
+                object = test_user_info.Buyer(dummy_email, dummy_name, dummy_pass, dummy_address,dummy_phonenum, dummy_ship)
+                object.putintofile()
+                
+         
+            else:
+                print("Input a Valid Number!")
+
+    elif MenuChoice == 2:
+        while True:
+            print("[1] Login")
+            print("[2] Sign-up")
+            SubMenuChoice = int(input("Choice: "))
+
+            if SubMenuChoice == 1:
+                dummy_name = input("Enter Username: ")
+                dummy_pass = input("Enter Password: ")
+                #reads the file for username and pass, if it has a match then login sucessful
+                for line in open("seller_db.txt", "r").readlines():
+                    print(line)
+                    signin_check = line.split()
+                    
+                    if dummy_name == signin_check[1] and dummy_pass == signin_check[2]:
+                        os.system('cls')
+                        print("Login Successful")
+                        break
+                    else:
+                        os.system('cls')
+                        print("Login Failed")
+
+
+            elif SubMenuChoice == 2:
+                #WALA PA MEMBER ID ITERATION
+                dummy_email = input("Enter your E-mail: ")
+                dummy_name = input("Enter your desired username: ")
+                dummy_pass = input("Enter your password: ")
+                dummy_address = input("Enter your address: ")
+                dummy_phonenum = input("Enter your phone number: ")
+                dummy_bank = input("Enter your bank account number: ")
+                dummy_route = input("Enter your bank routing number: ")
+                object2 = test_user_info.Seller(dummy_email, dummy_name, dummy_pass, dummy_address,dummy_phonenum, dummy_bank, dummy_route)
+                object2.putintofile()
+                print("Account Created!")
+                input("Press Enter to Continue...")
+                os.system('cls')
+                break
+            else:
+                print("Input a Valid Number!")
+
+    elif MenuChoice == 3:
+        pass
+        #NEED NETO NG FILE HANDLING TO BE COMPLETED
+    elif MenuChoice == 4:
+        pass
+        #NEED NETO NG FILE HANDLING TO BE COMPLETED / SI MAR DITO
+    elif MenuChoice == 5:
+        break
+    else:
+        print("Input a Valid Number!")
---
+++ test_user_info.py
@@ -0,0 +1,141 @@
+import os.path
+class Person():
+    __member_id=0
+    __id_member=0
+    def __init__(self,email_address,name,password,address,phonenum):
+        self.__member_id+=1
+        self.__id_member=self.__member_id
+        self.email_address=email_address
+        self.name=name
+        self.password=password
+        self.address=address
+        self.phonenum=phonenum
+
+    def show(self):
+        print(self.__id_member)
+        print(self.email_address)
+        print(self.name)
+        print(self.password)
+        print(self.address)
+        print(self.phonenum)
+        
+    def getter():
+        print(Person.__id_member)
+
+class Buyer(Person):
+    def __init__(self, email_address, name, password, address, phonenum,shipping_address):
+        super().__init__(email_address, name, password, address, phonenum)
+        self.shipping_address=shipping_address
+        self.account_type = "buyer"
+
+    # Gets all current attributes inputted at Menu.py, puts it into an array and writes the array into file
+    def putintofile(self):
+        arrayobj = [self.email_address, self.name, self.password, self.address, self.phonenum,self.shipping_address,self.account_type]
+        with open("buyer_db.txt","a") as file:
+            for line in arrayobj:
+                file.write(("".join(line)+" "))
+            file.write('\n')
+
+        
+
+    def show(self):
+        super().show()
+        print(self.shipping_address)
+
+class Seller(Person):
+    def __init__(self, email_address, name, password, address, phonenum,bankaccnum,routnum):
+        super().__init__(email_address, name, password, address, phonenum)
+        self.bankaccnum=bankaccnum
+        self.routnum=routnum
+        self.account_type = "seller"
+
+    # Gets all current attributes inputted at Menu.py, puts it into an array and writes the array into file
+    def putintofile(self):
+        arrayobj = [self.email_address, self.name, self.password, self.address, self.phonenum,self.bankaccnum,self.routnum,self.account_type]
+        with open("seller_db.txt","a") as file:
+            for line in arrayobj:
+                file.write(("".join(line)+" "))
+            file.write('\n')
+ 
+        
+
+    def show(self):
+        super().show()
+        print(self.bankaccnum)
+        print(self.routnum)
+        print(self.account_type)
+    
+        
+
+
+   
+
+
+
+# doing a test 
+'''
+def putinfile(info_array):
+ #Pass an array to this 
+ #Check if file exist otherwise it will create a new file
+ if os.path.isfile("demofile2.txt"):
+    f=open("demofile2.txt","a")
+    for person in range(len(info_array)):
+        g=info_array[person].get()
+        if info_array[person].member=="Buyer":
+            print("This is Append Buy")
+            f.write("Buyer")
+            f.write(str(g))
+            f.write("\n")
+        
+        else:
+            print("This is Append Sell")
+            f.write("Seller")
+            f.write(str(g))
+            f.write("\n")
+        
+ 
+    f.close()
+ else:
+    f=open("demofile2.txt","w")
+    for person in range(len(info_array)):
+        g=info_array[person].get()
+        if info_array[person].member=="Buyer":
+            print("This is Write Buy")
+            f.write("Buyer")
+            f.write(str(g))
+            f.write("\n")
+        
+        else:
+            print("This is Write Sell")
+            f.write("Seller")
+            f.write(str(g))
+            f.write("\n")
+        
+ 
+    f.close()
+
+def get_info():
+#Assign this to an array
+#Check if the file exist and read the content otherwise it return nothing
+   if os.path.isfile("demofile2.txt"):
+    info_array=[]
+    with open("demofile2.txt","r") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            else:
+             info_array.append(eval(line))
+    return info_array
+
+
+#removables
+info_array=get_info()
+if not info_array:
+    print("Empty Set")
+else: 
+    for info in info_array:
+        info.show()
+'''
+
+    
